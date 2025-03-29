56ecb93d56707ab2fb04fc00b0314bbf94ec64e3
Xavi Moll <xmollv@gmail.com>
2025-03-29 15:20:35 +0100

    Initial Commit

---
+++ Router/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Router
+//
+//  Created by Xavi Moll on 29/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by Xavi Moll on 29/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
